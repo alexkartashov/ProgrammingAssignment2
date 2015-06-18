4d69542f9577c4a415a4e79f0a063bdd0800c7aa
alexkartashov <alex.kartashov@gmail.com>
2015-06-18 14:16:31 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,53 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a list containing a function to
+# 1.set the value of the matrix
+# 2.get the value of the matrix
+# 3.set the value of the inverted matrix
+# 4.get the value of the inverted matrix
 
+
+makeCacheMatrix <- function(x = numeric()) {
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setInverse <- function(solve) m <<- solve
+      getInverse <- function() m
+      list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
+# This function first checks if the inverted matrix is already calculated.
+# If yes, it returns the inverted matrix and stops. If not, it calculates
+# The inverted matrix and returns is, and sets the flag for inverted matrix 
+# calculated 	
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getInverse()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setInverse(m)
+      m
 }
+
+
+# example of program working
+
+x<- matrix (c(100,0,0,100), nrow=2, ncol=2)
+print (x)
+z<-makeCacheMatrix (x)
+print (z)
+cacheSolve (z)
+# repeat to make sure cache works
+cacheSolve (z)
